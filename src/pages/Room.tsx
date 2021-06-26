42080ec3ba8aaf5aade507d029201b0f0f572a36
Douglas Strey <douglas.morini@hotmail.com>
2021-06-26 14:00:40 -0300

    Added the funcnionality to autenticate with google inside the room.

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -17,12 +17,20 @@ type RoomParams = {
 }
 
 export function Room() {
-    const { user } = useAuth();
+    const { user, signInWithGoogle } = useAuth();
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('');
     const roomId = params.id;
 
     const { questions, title } = useRoom(roomId);
+
+    async function handleSignIn() {
+        if (!user) {
+            await signInWithGoogle()
+        }
+
+        return;
+    }
 
     async function handleSendQuestion(event: FormEvent) {
         event.preventDefault();
@@ -89,7 +97,7 @@ export function Room() {
                                 <span>{user.name}</span>
                             </div>
                         ) : (
-                            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                            <span>Para enviar uma pergunta, <button onClick={handleSignIn}>faça seu login</button>.</span>
                         )}
                         <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
